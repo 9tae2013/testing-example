4f8d896f7d7174bb6e1579e12d9bb7bc501b4e23
piya_l <piya.tae@gmail.com>
2017-09-01 12:20:59 +0700

    Add test

--- src/main/java/test/major/service/TicketService.java
+++ src/main/java/test/major/service/TicketService.java
@@ -5,9 +5,23 @@ import test.major.domain.SelectedSeat;
 import java.util.List;
 
 public class TicketService {
+    private CardService cardService;
 
+    public TicketService(CardService cardService) {
+        this.cardService = cardService;
+    }
 
     public int calculateTotalPoint(List<SelectedSeat> selectedSeats) {
-        throw new UnsupportedOperationException("Not implement yet");
+        int total = 0;
+        for (SelectedSeat selectedSeat : selectedSeats) {
+            total += selectedSeat.getCount() * selectedSeat.getSeatType().getUsePoint();
+        }
+
+        if (total < cardService.getPoint(1)) {
+            return total;
+        } else {
+            return -1;
+        }
+
     }
 }
--- src/test/java/test/major/service/TicketServiceTest.java
+++ src/test/java/test/major/service/TicketServiceTest.java
@@ -1,15 +1,69 @@
 package test.major.service;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import test.major.domain.SeatType;
+import test.major.domain.SelectedSeat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 
+@RunWith(JUnitParamsRunner.class)
 public class TicketServiceTest {
+    @InjectMocks
+    TicketService ticketService;
+    @Mock
+    CardService cardService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
-    public void calculate_total_point() {
+    @Parameters({
+            "1, 1, 4000",
+            "2, 3, 11000"
+    })
+    public void returnZero_whenNotSelectSeat(int normal, int honeymoon, int expectedTotal) {
         //arrange
-
+        List<SelectedSeat> selectedSeats = createSelectedSeat(normal, honeymoon);
+        when(cardService.getPoint(anyInt())).thenReturn(1000000000);
         //act
-
+        int totalPoint = ticketService.calculateTotalPoint(selectedSeats);
         //assert
+        assertThat(totalPoint, equalTo(expectedTotal));
+    }
+
+    private List<SelectedSeat> createSelectedSeat(Integer normal, Integer honeymoon) {
+
+        List<SelectedSeat> selectedSeats = new ArrayList<>();
+        if (normal != null) {
+            SelectedSeat normalSeat = new SelectedSeat(
+                    new SeatType("Normal", 1_000),
+                    normal);
+            selectedSeats.add(normalSeat);
+        }
+
+        if (honeymoon != null) {
+            SelectedSeat honeymoonSeat = new SelectedSeat(
+                    new SeatType("Honeymoon", 3_000),
+                    honeymoon);
+            selectedSeats.add(honeymoonSeat);
+        }
+        return selectedSeats;
     }
 }
